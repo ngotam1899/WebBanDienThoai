636f718efe0d65f374720f69d3eb55a4f8a1b070
ngotam18991 <ngotam1899@gmail.com>
2020-09-25 22:47:03 +0700

    [FE] LoginPage
    Create a react app and a Login Page

---
+++ FE_WebBanDienThoai/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from "react-redux";
+import LoginPage from './components/LoginPage';
+
+function App() {
+  return (
+    <LoginPage/>
+  );
+}
+
+export default App;
---
+++ FE_WebBanDienThoai/src/components/LoginPage/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class LoginPage extends Component {
+	render() {
+		return (
+			<div>
+				<img class="wave" src="img/wave.png" alt="" />
+				<div class="container">
+					<div class="img">
+						<img src="img/bg.svg" alt="" />
+					</div>
+					<div class="login-content">
+						<form action="index.html">
+							<img src="img/avatar.svg" alt="" />
+							<h2 class="title">Welcome to Bookstore</h2>
+							<div class="input-div one">
+								<div class="i">
+									<i class="fas fa-user" />
+								</div>
+								<div class="div">
+									<h5>Username</h5>
+									<input type="text" class="input" />
+								</div>
+							</div>
+							<div class="input-div pass">
+								<div class="i">
+									<i class="fas fa-lock" />
+								</div>
+								<div class="div">
+									<h5>Password</h5>
+									<input type="password" class="input" />
+								</div>
+							</div>
+							<a href="#">Forgot Password?</a>
+							<input type="submit" class="btn" value="Login" />
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default LoginPage;
