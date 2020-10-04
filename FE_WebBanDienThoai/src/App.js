513f2bd4d3faf97c4b9b6de52fb27b8013274c06
ngotam18991 <ngotam1899@gmail.com>
2020-10-05 00:22:08 +0700

    [FE] HomePage ProductPage DetailPage

--- FE_WebBanDienThoai/src/App.js
+++ FE_WebBanDienThoai/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Provider } from "react-redux";
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
+import Header from './containers/Header';
 
 function App() {
   return (
-    <LoginPage/>
+    <Header/>
   );
 }
 
